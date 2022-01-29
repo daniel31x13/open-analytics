fdb66bc71526171e4565e0bf9e14ac59f8715678
Daniel31X13 <smalavi3@gmail.com>
2022-01-29 17:15:22 +0330

    Finished "hours of activity" component

--- dashboard/src/component/HoursOfActivityPerDay.js
+++ dashboard/src/component/HoursOfActivityPerDay.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -10,15 +9,37 @@ import {
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 
-const HoursOfActivityPerDay = () => {
-  const labels = ['Jan', 'Feb', 'May', 'Jun', 'Jul'];
+const HoursOfActivityPerDay = ({ activity }) => {
+  let rawData = activity.map((entity) => {
+    const date = new Date(entity.Date).toISOString().substring(0, 10)
+    const num = parseInt(entity.ActiveTimeInSecond);
+    const obj = { [date]: num }
+    return obj
+  });
+
+  let Data = {};
+
+  for(let i = 0; i < rawData.length; ++i){
+    for(let obj in rawData[i]){
+      Data[obj] = Data[obj] ? Data[obj] + rawData[i][obj] : rawData[i][obj];
+     }
+  }
+
+  
+
+  const date = Object.keys(Data);
+  const Sec = Object.values(Data).map((entity) => {
+    return (Math.round(entity / 60 * 100) / 100) 
+  });
+
+  const labels = date;
 
   const data = {
     labels,
     datasets: [
       {
-        label: 'Day',
-        data: [4,3,2,5,1],
+        label: 'Minutes',
+        data: Sec,
         backgroundColor: 'rgba(255, 99, 132, 0.8)',
       }
     ],
@@ -29,7 +50,7 @@ const HoursOfActivityPerDay = () => {
         data={data} 
         options={{ 
           maintainAspectRatio: false, 
-          plugins: { title: { display: true, text: 'Hours of activity per day:' }}
+          plugins: { title: { display: true, text: 'Activity per day:' }}
         }}
       />
   </div> );
