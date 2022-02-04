3788108fcd01e56dcb974890c6667ef7c335a9a1
Daniel31X13 <smalavi3@gmail.com>
2022-02-05 00:14:16 +0330

    BIG bug fix

--- dashboard/src/component/NewVsReturn.js
+++ dashboard/src/component/NewVsReturn.js
@@ -1,4 +1,3 @@
-// This chart is currently in beta, if there's a returning user in one day, there also has to be at least one new user in that day, and vice versa...
 import 'chart.js/auto';
 import { Line } from "react-chartjs-2";
 
@@ -6,7 +5,7 @@ const NewVsReturn = ({users}) => {
   let rawData = users.map((entity) => { // Parse the information so that only the Date (as the key) and the ip (as the value) is extracted
     const date = new Date(entity.Date).toISOString().substring(0, 10)
     const isNewUser = entity.isNewUser;
-    const obj = { date: date, newUser: isNewUser }
+    const obj = { x: date, y: +isNewUser }
     return obj;
   });
 
@@ -19,58 +18,43 @@ const NewVsReturn = ({users}) => {
 
   function newUsers() {
     let rawUsers = rawData.filter((entity) => {
-      return Boolean(entity.newUser)
+      return Boolean(entity.y)
     });
 
-    let users = rawUsers.map((entity) => {
-      let date = entity.date
-      return {[date]: 1}
-    })
-
-    let Data = {};
-
-    for(let i = 0; i < users.length; ++i){ // If the keys were the same then add the values together
-      for(let obj in users[i]){
-        Data[obj] = Data[obj] ? Data[obj] + users[i][obj] : users[i][obj];
-       }
+    for(const i in rawUsers) {
+      let counts = 0;
+      rawUsers.forEach((x) => {if (x.x === rawUsers[i].x) counts++});
+      rawUsers[i].y = counts;
     }
 
-    return Data
+
+    return rawUsers.filter((value, index, self) => index === self.findIndex((t) => t.x === value.x));
   }
 
   function returningUsers() {
     let rawUsers = rawData.filter((entity) => {
-      return !Boolean(entity.newUser)
+      return !Boolean(entity.y)
+    }).map((entity) => {
+      return { x: entity.x , y: 1 }
     });
 
-    let users = rawUsers.map((entity) => {
-      let date = entity.date
-      return {[date]: 1}
-    })
-
-    let Data = {};
-
-    for(let i = 0; i < users.length; ++i){ // If the keys were the same then add the values together
-      for(let obj in users[i]){
-        Data[obj] = Data[obj] ? Data[obj] + users[i][obj] : users[i][obj];
-       }
+    for(const i in rawUsers) {
+      let counts = 0;
+      rawUsers.forEach((x) => {if (x.x === rawUsers[i].x) counts++});
+      rawUsers[i].y = counts;
     }
-
-    return Data
+    
+    return rawUsers.filter((value, index, self) => index === self.findIndex((t) => t.x === value.x));
   }
 
-  // const newUserDates = Object.keys(newUsers());
-  const newUserValues = Object.values(newUsers());
-
-  // const returningUserDates = Object.keys(returningUsers());
-  const returningUserValues = Object.values(returningUsers());
+  console.log();
 
   const data = {
     labels: uniqueDates,
     datasets: [
       {
         label: "New Users",
-        data: newUserValues,
+        data: newUsers(),
         fill: true,
         backgroundColor: "rgba(75,192,192,0.2)",
         borderColor: "rgba(75,192,192,1)",
@@ -78,7 +62,7 @@ const NewVsReturn = ({users}) => {
       },
       {
         label: "Returning Users",
-        data: returningUserValues,
+        data: returningUsers(),
         fill: true,
         borderColor: "#742774",
         tension: 0.2
