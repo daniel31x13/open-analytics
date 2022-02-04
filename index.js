f4c67daff928a018b07eea5e443916fa3111ed78
fabi.stehle <fabian.stehle@informatik.hs-fulda.de>
2022-02-04 22:08:01 +0100

    Prettier used on index.js

--- index.js
+++ index.js
@@ -1,86 +1,112 @@
-const assignUser = require('./lib/assignUser');
-const geoip = require('fast-geoip');
-const express = require('express');
-const { MongoClient } = require('mongodb');
+const assignUser = require("./lib/assignUser");
+const geoip = require("fast-geoip");
+const express = require("express");
+const { MongoClient } = require("mongodb");
 const app = express();
 
-const http = require('http').createServer(app);
-const io = require('socket.io')(http, {
-    cors: {
-      origin: '*',
-    },
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+  },
 });
 
-const cors = require('cors');
+const cors = require("cors");
 
 const PORT = process.env.PORT || 8080; // Edit port if needed
 let count;
 let totalClients = [];
 
 // Connect to MongoDB
-const uri = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"; // Edit accordingly
+const uri =
+  "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"; // Edit accordingly
 const MClient = new MongoClient(uri);
 MClient.connect().catch(console.error);
 
 let results;
 app.use(async (req, res, next) => {
-    let threeMonthBefore = new Date(new Date().getFullYear(), new Date().getMonth() - 1, new Date().getDate()); // Last 14 days...
-    results = await MClient.db("DatabaseName").collection("CollectionName").find().toArray(); // Edit 'DatabaseName' & 'CollectionName' accordingly
-    next();
+  let threeMonthBefore = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth() - 1,
+    new Date().getDate()
+  ); // Last 14 days...
+  results = await MClient.db("DatabaseName")
+    .collection("CollectionName")
+    .find()
+    .toArray(); // Edit 'DatabaseName' & 'CollectionName' accordingly
+  next();
 });
 
-app.get('/api', cors(), (req, res) => {
-    res.send(results);
+app.get("/api", cors(), (req, res) => {
+  res.send(results);
 });
 
-app.get('/api/active', cors(), (req, res) => {
-    res.send(count.toString());
+app.get("/api/active", cors(), (req, res) => {
+  res.send(count.toString());
 });
 
-io.on('connection', async (socket) => { // On connection
-    let clientIp = socket.request.connection.remoteAddress;
-    let clientHeader = socket.request.headers['user-agent'];
-    let geo = await geoip.lookup(clientIp.slice(7)); // Check location by ip (Does not work for local ip addresses)
-    let clientURL;
-    let clientReferrer;
-    let isNewUser;
+io.on("connection", async (socket) => {
+  // On connection
+  let clientIp = socket.request.connection.remoteAddress;
+  let clientHeader = socket.request.headers["user-agent"];
+  let geo = await geoip.lookup(clientIp.slice(7)); // Check location by ip (Does not work for local ip addresses)
+  let clientURL;
+  let clientReferrer;
+  let isNewUser;
 
-    totalClients.push(clientIp); // Add user to array
+  totalClients.push(clientIp); // Add user to array
 
-    count = totalClients.filter(function(item, pos) { return totalClients.indexOf(item) == pos }).length; // The number of unique clients from the array
+  count = totalClients.filter(function (item, pos) {
+    return totalClients.indexOf(item) == pos;
+  }).length; // The number of unique clients from the array
 
-    const connectDate = new Date();
+  const connectDate = new Date();
+
+  console.clear(); // Clear previous log
+  console.log("Total Clients: " + count);
+
+  io.emit("socketClientID", socket.client.id);
+  socket.on("clientMessage", (data) => {
+    // Get url from client
+    clientURL = data.url;
+    clientReferrer = data.referrer;
+    isNewUser = data.isFirstVisit;
+    isNewUser = data.isFirstVisit;
+  });
+
+  socket.on("disconnect", () => {
+    // On disconnection
+    const disconnectDate = new Date();
+    let activeTime = Math.ceil(
+      (disconnectDate - connectDate) / 1000
+    ).toString();
+
+    totalClients.splice(totalClients.indexOf(clientIp), 1); // Remove user from array
+    count = totalClients.filter(function (item, pos) {
+      return totalClients.indexOf(item) == pos;
+    }).length; // Update the number of unique clients from the array
+
+    let user = assignUser(
+      clientIp,
+      geo.country,
+      clientHeader,
+      clientURL,
+      clientReferrer,
+      connectDate,
+      activeTime,
+      isNewUser
+    );
+
+    // Add to DB
+    MClient.db("DatabaseName").collection("CollectionName").insertOne(user); // Edit 'DatabaseName' & 'CollectionName' accordingly
 
     console.clear(); // Clear previous log
-    console.log('Total Clients: ' + count);
 
-    io.emit('socketClientID', socket.client.id);
-    socket.on('clientMessage', (data) => { // Get url from client
-        clientURL = data.url;
-        clientReferrer = data.referrer;
-        isNewUser = data.isFirstVisit;
-        isNewUser = data.isFirstVisit;
-    });
-
-    socket.on('disconnect', () => { // On disconnection
-        const disconnectDate = new Date()
-        let activeTime = Math.ceil((disconnectDate-connectDate)/1000).toString();
-
-        totalClients.splice(totalClients.indexOf(clientIp), 1); // Remove user from array
-        count = totalClients.filter(function(item, pos) { return totalClients.indexOf(item) == pos }).length; // Update the number of unique clients from the array
-    
-        let user = assignUser(clientIp, geo.country, clientHeader, clientURL, clientReferrer, connectDate, activeTime, isNewUser);
-
-        // Add to DB
-        MClient.db("DatabaseName").collection("CollectionName").insertOne(user); // Edit 'DatabaseName' & 'CollectionName' accordingly
-
-        console.clear(); // Clear previous log
-        
-        console.log('Total Clients: ' + count);
-    });
+    console.log("Total Clients: " + count);
+  });
 });
 
 http.listen(PORT, () => {
-    console.clear();
-    console.log("ALL SET!");
+  console.clear();
+  console.log("ALL SET!");
 });
