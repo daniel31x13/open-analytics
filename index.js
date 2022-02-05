d2e922a82e34803dee675409471223c81e0ddab2
Daniel <smalavi3@gmail.com>
2022-02-05 13:17:55 +0330

    Update index.js

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ app.get("/api/active", cors(), (req, res) => {
   res.send(count.toString());
 });
 
+// On connection
 io.on("connection", async (socket) => {
-  // On connection
   let clientIp = socket.request.connection.remoteAddress;
   let clientHeader = socket.request.headers["user-agent"];
   let geo = await geoip.lookup(clientIp.slice(7)); // Check location by ip (Does not work for local ip addresses)
@@ -53,37 +53,38 @@ io.on("connection", async (socket) => {
   let clientReferrer;
   let isNewUser;
 
-  totalClients.push(clientIp); // Add user to array
+  // Add user to array
+  totalClients.push(clientIp);
 
+  // The number of unique clients from the array
   count = totalClients.filter(function (item, pos) {
     return totalClients.indexOf(item) == pos;
-  }).length; // The number of unique clients from the array
+  }).length;
 
   const connectDate = new Date();
 
   console.clear(); // Clear previous log
-  console.log("Total Clients: " + count);
 
   io.emit("socketClientID", socket.client.id);
-  socket.on("clientMessage", (data) => {
-    // Get url from client
+  socket.on("clientMessage", (data) => { // Get data from client
     clientURL = data.url;
     clientReferrer = data.referrer;
     isNewUser = data.isFirstVisit;
     isNewUser = data.isFirstVisit;
   });
 
+  // On disconnection
   socket.on("disconnect", () => {
-    // On disconnection
     const disconnectDate = new Date();
     let activeTime = Math.ceil(
       (disconnectDate - connectDate) / 1000
     ).toString();
 
-    totalClients.splice(totalClients.indexOf(clientIp), 1); // Remove user from array
+    // Remove user from array & update the number of unique clients from the array
+    totalClients.splice(totalClients.indexOf(clientIp), 1);
     count = totalClients.filter(function (item, pos) {
       return totalClients.indexOf(item) == pos;
-    }).length; // Update the number of unique clients from the array
+    }).length;
 
     let user = assignUser(
       clientIp,
@@ -98,10 +99,6 @@ io.on("connection", async (socket) => {
 
     // Add to DB
     MClient.db("DatabaseName").collection("CollectionName").insertOne(user); // Edit 'DatabaseName' & 'CollectionName' accordingly
-
-    console.clear(); // Clear previous log
-
-    console.log("Total Clients: " + count);
   });
 });
 
